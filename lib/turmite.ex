14366772caf51585884e769f7c22249c083df079
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2017-03-02 20:30:51 -0300

    Generated code

---
+++ lib/turmite.ex
@@ -0,0 +1,18 @@
+defmodule Turmite do
+  @moduledoc """
+  Documentation for Turmite.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Turmite.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/turmite_test.exs
@@ -0,0 +1,8 @@
+defmodule TurmiteTest do
+  use ExUnit.Case
+  doctest Turmite
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
