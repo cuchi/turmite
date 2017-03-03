7f1669cf50df3c7368a5a7b1b6cd1e6cc7afee18
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2017-03-02 21:30:33 -0300

    Grid functions

--- lib/turmite.ex
+++ lib/turmite.ex
@@ -1,18 +1,40 @@
 defmodule Turmite do
-  @moduledoc """
-  Documentation for Turmite.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Turmite.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def create_grid(size) do
+    for _ <- 1..size do
+      List.duplicate 0, size
+    end
   end
+
+  defp pretty_line(line) do
+    Enum.join(line, " ")
+  end
+
+  def show_grid(grid) do
+    for line <- grid do
+      IO.puts (pretty_line line)
+    end
+    :ok
+  end
+
+  def center(grid) do
+    case is_list(grid) && div(Enum.count(grid), 2) do
+      x when x == false -> :invalid_grid
+      x when x > 0 -> {x, x}
+      _ -> :invalid_grid
+    end
+  end
+
+  def get(grid, x, y) do
+    grid
+      |> Enum.at(x)
+      |> Enum.at(y)
+  end
+
+  def set(grid, x, y, val) do
+    List.update_at(grid, x, fn line ->
+      List.replace_at(line, y, val) 
+    end)
+  end
+
 end
